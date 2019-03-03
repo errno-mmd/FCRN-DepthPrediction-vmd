045f27461eaaf6b04cb6f67473990d6505422b7d
miu <garnet200521358@gmail.com>
2019-03-03 23:20:32 +0900

    複数人数のトレース対応中

--- tensorflow/predict_video.py
+++ tensorflow/predict_video.py
@@ -408,6 +408,7 @@ def predict_video(now_str, model_path, video_path, depth_path, interval, openpos
             for i in range(len(data["people"]), people_size):
                 # 足りない分は空データを埋める
                 data["people"].append(json.load(open("tensorflow/json/one_keypoints.json")))
+                org_data["people"].append(json.load(open("tensorflow/json/one_keypoints.json")))
 
             # インデックス出力 ------------------------------
             if _len <= 0:
